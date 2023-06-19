1b1036a1544f05c7507ada8e0b5ad63e66dca04a
120EE0980 <120EE0980@nitrkl.ac.in>
2023-06-19 21:51:49 +0530

    Add recursion(bubblesort problem) revision june 19

--- src/src/Sorting/BubbleSort.java
+++ src/src/Sorting/BubbleSort.java
@@ -48,9 +48,9 @@ public class BubbleSort {
 
     static void bubble(int[] arr) {
         boolean swapped;
-      for (int i = 0; i < arr.length; i++) {
+      for (int i = 0; i < arr.length; i++) { //for every pass,highest value comes at end
           swapped=false;
-          for (int j = 1; j < arr.length-i ; j++) {  //for every pass,highest value comes at end
+          for (int j = 1; j < arr.length-i ; j++) {
             if(arr[j]<arr[j-1]){
                 int temp=arr[j];
                 arr[j]=arr[j-1];
